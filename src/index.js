a37f0d85f787fe84a702870bf448a442da2d4c8e
gpfksdlrn <gpfksdlrn@naver.com>
2022-04-12 16:20:07 +0900

    1.01 Initialization

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
+import firebase from "./firebase";
+console.log(firebase);
 
-ReactDOM.render(
+/*ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
+);*/
+const rootNode = document.getElementById('root');
+
+ReactDOM.createRoot(rootNode).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 );
